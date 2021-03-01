5e0460beb00f9d5ec71483e3b2d10287aab06bde
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-03-01 11:07:52 -0500

    start contract working example

--- zinc_base_spike.py
+++ zinc_base_spike.py
@@ -1,10 +1,30 @@
 from zincbase import KB
 
-
+### A simple case of zinc KB
+print('============================== kb.1')
 # https://zincbase.readthedocs.io/en/latest/README.html#installation
-# try a working knowledge base - simple case
-kb = KB()
-kb.store('eats(cat, cheese)')
+kb1 = KB()
+kb1.store('eats(cat, cheese)')
 
-for ans in kb.query('eats(cat, Food)'):
+for ans in kb1.query('eats(cat, Food)'):
     print(ans['Food'])
+
+###
+
+
+print('============================== kb.2')
+
+### A trial run for contract KB
+
+# 'amanda' is the license owner and grantor
+# 'billy' is the the grantee / user
+kb = KB()
+
+# make a fact
+kb.store('grants(@amanda, license')
+kb.store('receives(@billy, license')
+
+kb.store('isa(license, mitLicense')
+
+
+###
