f1b370ea973b950fb66737536a62b6fa78d3d6d2
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-03-01 22:49:32 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+# this area is just for debugging etc
+# scrips are called by app from application
 from knowledge_base.kb_license_query import all_terms_conditions
 
-all_terms_conditions('MIT License')
+all_terms_conditions('mit_license')
