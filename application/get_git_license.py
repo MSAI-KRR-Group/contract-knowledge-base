0a1aa8cd9cc0bcf6d372ca8e082b5e46dc06d6e3
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-03-07 14:16:53 -0500

    complete feature request
    manual synch for masum feature get repo url

---
+++ application/get_git_license.py
@@ -0,0 +1,16 @@
+import urllib.request
+from bs4 import BeautifulSoup
+
+def get_license_name(url):
+    page = urllib.request.urlopen(url).read()
+    soup = BeautifulSoup(page, features="html.parser")
+
+    for svg in soup.find_all('svg'):
+        if "octicon-law" in svg.get("class"):
+            license_name = svg.parent.text.replace(" ", "").replace("\n", "")
+            if "MIT" in license_name:
+                return "mit_license"
+            elif "GPL" in license_name:
+                return "gnu_license"
+            else:
+                return "<github_repo>"
