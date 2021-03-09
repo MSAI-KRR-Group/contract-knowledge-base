06f2bc00de704aa131dc73ee2be3f0a034a14869
justinhchae <justin.chae@nlaw.northwestern.edu>
2021-03-08 20:09:52 -0500

    update citation

--- application/load_css.py
+++ application/load_css.py
@@ -1,6 +1,6 @@
 from application.config import *
 
-
+# derived from https://discuss.streamlit.io/t/colored-boxes-around-sections-of-a-sentence/3201/2
 def local_css(file_name):
     with open(file_name) as f:
         st.markdown('<style>{}</style>'.format(f.read()), unsafe_allow_html=True)
